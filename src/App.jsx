99d6415d1611624ff107bf848ac12e95a9b1ec3d
amorlett2021 <beardencreative1@gmail.com>
2025-07-12 14:28:52 -0500

    fix grammar errors and gsap erros clean up code to make it readable for everyone including myself

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-
-import gsap from "gsap";
+import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from "gsap/all";
-import Navbar from "./components/Navbar.jsx";
 
-gsap.registerPlugin(ScrollTrigger,SplitText);
+import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
     return (
     <main>
 
-    <Navbar />
+            <Navbar />
+            <Hero />
+    <div className="h-dvh bg-black"></div>
     </main>
     )
 }
